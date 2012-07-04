a1e4cc7d38c70d9cfd8c911e6544c27f20b9f3ed
Takeshi Yabe <tyabe@nilidea.com>
2012-07-04 15:23:22 +0900

    add routes of vote

--- app/controllers/nominee.rb
+++ app/controllers/nominee.rb
@@ -10,4 +10,22 @@ KajaNomination.controllers :nominee do
     render 'nominees/show'
   end
 
+  get :new, map: '/nominee/:account/vote' do
+    @nominee = Nominee.find_by_account(params[:account])
+    @ballot = @nominee.ballots.new(user: current_user)
+    render 'nominees/vote'
+  end
+
+  post :create, map: '/nominee/:account/vote' do
+    @nominee = Nominee.find_by_account(params[:account])
+    @ballot = @nominee.ballots.new(params[:ballot].merge(user: current_user))
+    if @ballot.save
+      flash[:notice] = 'Vote was successfully created.'
+      redirect url(:nominee, :show, account: @nominee.account)
+    else
+      render 'nominees/vote'
+    end
+
+  end
+
 end
