ae3b740e256943236a64219c232b069b5d3812b8
Takeshi Yabe <tyabe@nilidea.com>
2012-07-06 02:32:50 +0900

    adjusted the wording

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 KajaNomination.helpers do
 
+  def title
+    [Setting.site.community, Setting.site.title].join(" | ")
+  end
+
+  def site_title
+    [Setting.site.title, Setting.site.community].join(" of ")
+  end
+
   def alert_message(name)
     "alert-" <<
       case name.to_sym
