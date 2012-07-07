d2d9589efa75019bc4f5ae75942fbad41e8cccfe
Takeshi Yabe <tyabe@nilidea.com>
2012-07-08 04:12:26 +0900

    cast the return value of the auth

--- app/app.rb
+++ app/app.rb
@@ -28,7 +28,7 @@ class KajaNomination < Padrino::Application
 
   get :auth, map: '/auth/:provider/callback' do
     auth    = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by_provider_and_uid(auth["provider"].to_s, auth["uid"].to_s) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     flash[:notice] = t('app.login_success')
     redirect session.delete(:return_to) || '/'
